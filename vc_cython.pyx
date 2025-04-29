ef1d5f3dca644353a267b41df4a85661e6afd726
Juan Luque <juand1121@gmail.com>
2025-04-29 13:09:58 -0400

    Fix number of edges in cython edge_list creation

--- vc_cython.pyx
+++ vc_cython.pyx
@@ -16,19 +16,20 @@ cnp.import_array()
 
 DTYPE = np.int64
 ctypedef cnp.int64_t DTYPE_t
+ctypedef pair[pair[int, int], pair[int, int]] pair_of_pairs_t
 
-cdef vector[pair[int, int]] get_conflict_edgelist(cnp.ndarray[DTYPE_t, ndim=2] A) noexcept:
+cdef vector[pair_of_pairs_t] get_conflict_edgelist(cnp.ndarray[DTYPE_t, ndim=2] A) noexcept:
     """Given a matrix A of mutations and cell samples, return an edgelist of
     corresponding to a vertex cover instance of the matrix A. I.e., edges occur
     between zeros in conflict.
     """
     cdef int n = A.shape[1]
     cdef int m = A.shape[0]
-    cdef vector[pair[int, int]] edge_list 
+    cdef vector[pair_of_pairs_t] edge_list 
 
     cdef int p, q
     cdef int row1, row2
-    cdef int has_one_one
+    cdef int has_one_one = 0
     cdef vector[int] zero_ones, one_zeros
 
     for p in range(n):
@@ -36,40 +37,47 @@ cdef vector[pair[int, int]] get_conflict_edgelist(cnp.ndarray[DTYPE_t, ndim=2] A
             for i in range(m):
                 if A[i, p] and A[i, q]:
                     has_one_one = 1
-                elif A[i, p] and not A[i, q]:
+                elif A[i, p] == 1 and A[i, q] == 0:
                     one_zeros.push_back(i)
-                elif not A[i, p] and A[i, q]:
+                elif A[i, p] == 0 and A[i, q] == 1:
                     zero_ones.push_back(i)
 
-            if not has_one_one:
-                continue
-
-            for row1 in zero_ones:
-                for row2 in one_zeros:
-                    edge_list.push_back(pair[int, int](row1, p))
-                    edge_list.push_back(pair[int, int](row2, q))
+            if has_one_one == 1:
+                for row1 in zero_ones:
+                    for row2 in one_zeros:
+                        edge_list.push_back(
+                                pair_of_pairs_t(pair[int, int](row1, p), pair[int, int] (row2, q))
+                        )
+                        # edge_list.push_back(pair[int, int](row2, q))
             one_zeros.clear()
             zero_ones.clear()
+            has_one_one = 0
 
+    print("Edge list size: ", len(edge_list))
     return edge_list
 
-cdef int min_unweighted_vertex_cover_from_edgelist(vector[pair[int, int]] edge_list) noexcept:
+cdef int min_unweighted_vertex_cover_from_edgelist(vector[pair_of_pairs_t] edge_list) noexcept:
     """For unweightred case, no need to use local ratio techniques used in
     networkx.algorithms.min_weighted_vertex_cover().
     """
+    # Will hash the pairs manualy
     cdef unordered_set[int] cover
 
     # TODO: Use a random device and swaps to shuffle the array instead of calling to numpy
     cdef int num_edges = edge_list.size()
     cdef cnp.ndarray[DTYPE_t, ndim=1] ixs = np.random.permutation(num_edges)
-    cdef int i, u, v
+    cdef int i, lrow, lcol, rrow, rcol
+    cdef pair[int, int] lpair, rpair
+    cdef int lhash, rhash
     for i in range(num_edges):
         # u, v = edge_list[i]
-        u = edge_list[ixs[i]].first
-        v = edge_list[ixs[i]].second
-        if cover.find(u) == cover.end() or cover.find(v) == cover.end():
-            cover.insert(u)
-            cover.insert(v)
+        lpair = edge_list[ixs[i]].first
+        rpair = edge_list[ixs[i]].second
+        lhash = lpair.first * num_edges + lpair.second
+        rhash = rpair.first * num_edges + rpair.second
+        if cover.find(lhash) == cover.end() or cover.find(rhash) == cover.end():
+            cover.insert(lhash)
+            cover.insert(rhash)
 
     if cover.size() % 2 == 0:
         return cover.size() // 2
@@ -178,7 +186,7 @@ def get_bounds(cnp.ndarray[DTYPE_t, ndim=2] A, int iterations = 1):
     cdef int best_lb = 0
     cdef int best_ub = INT_MAX
     cdef int i, lb, greedy_ub
-    cdef vector[pair[int, int]] edge_list = get_conflict_edgelist(A)
+    cdef vector[pair_of_pairs_t] edge_list = get_conflict_edgelist(A)
     cdef vector[pair[int, int]] flips
     for i in range(iterations):
         # Have the second returned value be the upper bound
