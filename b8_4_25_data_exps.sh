28d94d7e7865808fdd269be8805f796f30c1cd69
Juan Luque <juand1121@gmail.com>
2025-05-04 17:03:49 -0400

    Add experiment bash scripts for each bounding algorithm

---
+++ b8_4_25_data_exps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --time=2-00:00:00
+#SBATCH --qos=medium
+#SBATCH --mem=64gb
+#SBATCH --output=phiscs.out.%j
+#SBATCH --error=phiscs.out.%j
+#SBATCH --cpus-per-task 8
+
+srun lscpu | grep 'Model name'
+
+algorithm=8
+
+for file in $(ls -Sr data_4_28_25/*.SC.after_noise);
+do
+  python main.py --b $algorithm --input $file --output solutions/b$algorithm --log b${algorithm}.log
+done
