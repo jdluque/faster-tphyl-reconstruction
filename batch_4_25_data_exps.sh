59d4170eb17ba6b42fac42d7a04668c9bcc66127
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2025-05-06 16:50:17 -0400

    Update Bash script: Only run the experiment if it hasn't been run before

--- batch_4_25_data_exps.sh
+++ batch_4_25_data_exps.sh
@@ -11,7 +11,39 @@ algorithm=$1
 
 srun lscpu | grep 'Model name'
 
-for file in $(ls -Sr data_4_28_25/*.SC.after_noise)
+times_file="experiments/times.csv"
+log_file="skipped.log"
+
+alg_names=(
+  "TwoSat full"
+  "TwoSat compact"
+  "LP GLOP full rewrite"
+  "LP PDLP partial rewrite"
+  "LP Gurobi partial rewrite"
+  "LP PDLP full rewrite"
+  "Vertex Cover"
+  "Maximum Weight Matching"
+  "Hybrid LP Gurobi partial rewrite"
+  "Hybrid LP PDLP full rewrite"
+  "Hybrid LP PDLP partial rewrite"
+)
+
+alg_index=$((algorithm - 1))
+alg_name="${alg_names[$alg_index]}"
+
+# Ensure skipped.log exists
+touch "$log_file"
+
+for file in $(ls -Sr data_4_28_25/*.SC.after_noise);
 do
+  dataset_name=$(basename "$file") # removes the .after_noise part
+
+  # Check if this algorithm and dataset combination has done
+  if grep -q "^$alg_name,$dataset_name," "$times_file";
+  then
+    echo "$(date): $alg_name skipped $dataset_name" >> "$log_file"
+    continue
+  fi
+
   srun --time=2:00:00 -- python main.py --b $algorithm --input $file --output solutions/b$algorithm --log b${algorithm}.log
 done
