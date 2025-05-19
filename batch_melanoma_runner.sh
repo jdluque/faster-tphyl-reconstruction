9d34880fd3dfe8665ebcaa4d7db159a1c6f764f3
Juan Luque <juand1121@gmail.com>
2025-05-19 12:55:48 -0400

    Squashed commit of the following:
    commit 02b895ae90b641421cc6bbdf9aed62312861aeb1
    Author: Juan Luque <juand1121@gmail.com>
    Date:   Mon May 19 12:49:58 2025 -0400
    
        Update example data outputs.
    
        The outputs now come from the matrices with no NAs.
    
    commit 9f7d9e573a475bc3acd017c980c73332e520e337
    Author: Juan Luque <juand1121@gmail.com>
    Date:   Mon May 19 12:47:58 2025 -0400
    
        Update to use `data1.SC` with no NAs
    
    commit 057e12e16ffa603d377a3251392e77d88caa9808
    Author: Juan Luque <juand1121@gmail.com>
    Date:   Mon May 19 12:44:00 2025 -0400
    
        Clean up bash runner and prototype script

--- batch_melanoma_runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for algorithm in 1 9 10 11
-do
-  sbatch --job-name b${algorithm}_melanoma -- batch_melanoma_exps.sh $algorithm
-done
--- batch_runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for algorithm in 2 9 10 11
-do
-  sbatch --job-name b${algorithm}_phiscs -- batch_4_25_data_exps.sh $algorithm
-done
