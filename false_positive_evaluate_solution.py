e0b0d8345f23f83e873419cb7c49b228bedc999b
Juan Luque <juand1121@gmail.com>
2025-12-14 17:16:07 -0800

    Write different lineage accuracy values to false_positive_results.csv

--- false_positive_evaluate_solution.py
+++ false_positive_evaluate_solution.py
@@ -1,7 +1,10 @@
-import os
-import pandas as pd
-import numpy as np
 import argparse
+import os
+
+import numpy as np
+import pandas as pd
+
+from different_lineage import different_lineage
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_file", required=True)
@@ -23,18 +26,22 @@ else:
 
 # Determine the sim_name and before noise file
 if ".false_positives" in filename:
-    sim_name = filename.replace(".false_positives", "").replace(f"-fp_{fp_probability}", "-fp_0")
+    sim_name = filename.replace(".false_positives", "").replace(
+        f"-fp_{fp_probability}", "-fp_0"
+    )
 else:
     sim_name = filename.replace(".after_noise", "")
 before_noise_file = sim_name + ".before_noise"
 
 # Read in the before noise data
-before_noise_df = pd.read_csv(os.path.join(args.original, before_noise_file), sep="\t", header=None)
+before_noise_df = pd.read_csv(
+    os.path.join(args.original, before_noise_file), sep="\t", header=None
+)
 before_noise_df = before_noise_df.iloc[:, 1:].iloc[1:, :].astype(int)
 
 # Read in the solution data
 solution_file = os.path.join(args.solution, os.path.splitext(filename)[0] + ".CFMatrix")
-if os.path.exists(solution_file): # if this solution file exists
+if os.path.exists(solution_file):  # if this solution file exists
     solution_df = pd.read_csv(solution_file, sep="\t", header=None)
     solution_df = solution_df.iloc[:, 1:].iloc[1:, :].astype(int)
 
@@ -44,13 +51,28 @@ if os.path.exists(solution_file): # if this solution file exists
     # Comparison metric 2: what percentage of pairs in the ground truth that have ancestor-descendant relationship are also ancestor-descendant in our solution matrix
     ancestor_descendant_pair_count = 0
     consistent_pair_count = 0
-    for i in range(before_noise_df.shape[1]): # hypothetical decendant
-        for j in range(i + 1, before_noise_df.shape[1]): # hypothetical ancestor
-            if not np.any(before_noise_df.iloc[:, i].values.view(bool) & ~before_noise_df.iloc[:, j].values.view(bool)): # Cell that has i but not j
+    for i in range(before_noise_df.shape[1]):  # hypothetical decendant
+        for j in range(i + 1, before_noise_df.shape[1]):  # hypothetical ancestor
+            if not np.any(
+                before_noise_df.iloc[:, i].values.view(bool)
+                & ~before_noise_df.iloc[:, j].values.view(bool)
+            ):  # Cell that has i but not j
                 ancestor_descendant_pair_count += 1
-                if not np.any(solution_df.iloc[:, i].values.view(bool) & ~solution_df.iloc[:, j].values.view(bool)):
+                if not np.any(
+                    solution_df.iloc[:, i].values.view(bool)
+                    & ~solution_df.iloc[:, j].values.view(bool)
+                ):
                     consistent_pair_count += 1
-else: # Assume that the simulation timed out
+
+    different_lineage_numerator, different_lineage_denominator = different_lineage(
+        solution_df.values, before_noise_df.values
+    )
+    different_lineage_accuracy = (
+        different_lineage_numerator / different_lineage_denominator
+        if different_lineage_denominator
+        else 1
+    )
+else:  # Assume that the simulation timed out
     l1_distance = -1
     ancestor_descendant_pair_count = -1
     consistent_pair_count = -1
@@ -63,18 +85,25 @@ num_false_positives = (generated_df > before_noise_df).sum().sum()
 num_existing_false_negatives = (generated_df < before_noise_df).sum().sum()
 
 # Append to CSV
-results_df = pd.DataFrame([{
-    "sim_name": sim_name,
-    "algorithm": args.algorithm,
-    "fp_probability": fp_probability,
-    "num_rows": before_noise_df.shape[0],
-    "num_cols": before_noise_df.shape[1],
-    "num_existing_false_negatives": num_existing_false_negatives,
-    "num_false_positives": num_false_positives,
-    "l1_distance": l1_distance,
-    "total_ancestor_descendant_pairs": ancestor_descendant_pair_count,
-    "consistent_pair_count": consistent_pair_count,
-    "filename": filename,
-    "runtime": args.runtime
-}])
+results_df = pd.DataFrame(
+    [
+        {
+            "sim_name": sim_name,
+            "algorithm": args.algorithm,
+            "fp_probability": fp_probability,
+            "num_rows": before_noise_df.shape[0],
+            "num_cols": before_noise_df.shape[1],
+            "num_existing_false_negatives": num_existing_false_negatives,
+            "num_false_positives": num_false_positives,
+            "l1_distance": l1_distance,
+            "total_ancestor_descendant_pairs": ancestor_descendant_pair_count,
+            "consistent_pair_count": consistent_pair_count,
+            "different_lineage_numerator": different_lineage_numerator,
+            "different_lineage_denominator": different_lineage_denominator,
+            "different_lineage_accuracy": different_lineage_accuracy,
+            "filename": filename,
+            "runtime": args.runtime,
+        }
+    ]
+)
 results_df.to_csv(args.results, mode="a", header=False, index=False)
